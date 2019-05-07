ef38a82ee6608bd974e9983e0afacd6abc9931b8
Peter Ansell <p_ansell@yahoo.com>
2019-05-07 10:20:45 +1000

    Use HTTPS and new hostnames where possible

--- grails-app/controllers/au/org/ala/ozatlasproxy/ProxyController.groovy
+++ grails-app/controllers/au/org/ala/ozatlasproxy/ProxyController.groovy
@@ -172,7 +172,7 @@ class ProxyController {
     def image = {
         def out = response.getOutputStream()
         try {
-            def url = ("http://bie.ala.org.au/ws/species/image/" + params.imageType + "/" + params.guid).toURL()
+            def url = ("https://bie-ws.ala.org.au/ws/species/image/" + params.imageType + "/" + params.guid).toURL()
             url.getBytes()
             out.write(url.getBytes())
             out.flush()
@@ -184,62 +184,62 @@ class ProxyController {
     }
 
     def geocode = {
-        def url = ("http://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=" + params.latlng).toURL()
+        def url = ("https://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=" + params.latlng).toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def placeSearch = {
-        def url = ("http://maps.googleapis.com/maps/api/geocode/json?sensor=true&address=" + params.address.encodeAsURL()).toURL()
+        def url = ("https://maps.googleapis.com/maps/api/geocode/json?sensor=true&address=" + params.address.encodeAsURL()).toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def exploreGroups = {
-        def url = ("http://biocache.ala.org.au/ws/explore/groups.json?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius).toURL()
+        def url = ("https://biocache-ws.ala.org.au/ws/explore/groups.json?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius).toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def search = {
-        def url = ("http://bie.ala.org.au/ws/search.json?fq=${params.fq}&pageSize=${params.pageSize}&q=${params.q}").toURL()
+        def url = ("https://bie-ws.ala.org.au/ws/search.json?fq=${params.fq}&pageSize=${params.pageSize}&q=${params.q}").toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def species = {
-        def url = ("http://bie.ala.org.au/ws/species/${params.id}").toURL()
+        def url = ("https://bie-ws.ala.org.au/ws/species/${params.id}").toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def exploreTaxonGroup = {
-        def url = ("http://biocache.ala.org.au/ws/occurrence/search" + params.group + "?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + "&common=" + params.common).toURL()
+        def url = ("https://biocache-ws.ala.org.au/ws/occurrence/search" + params.group + "?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + "&common=" + params.common).toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def exploreGroup = {
-        def url = ("http://biocache.ala.org.au/ws/explore/group/" + params.group + "?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + "&common=" + params.common).toURL()
+        def url = ("https://biocache-ws.ala.org.au/ws/explore/group/" + params.group + "?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + "&common=" + params.common).toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def exploreGroupWithGallery = {
-        def url = ("http://biocache.ala.org.au/ws/explore/group/" + params.group + "?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + "&common=" + params.common).toURL()
+        def url = ("https://biocache-ws.ala.org.au/ws/explore/group/" + params.group + "?fq=geospatial_kosher%3Atrue&facets=species_group&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + "&common=" + params.common).toURL()
 
         try {
             def slurper = new JsonSlurper()
             def result = slurper.parseText(url.getText())
             def guids = result.collect { x -> x.guid }
 
-            // do a HTTP POST to http://bie.ala.org.au/species/bulklookup with a JSON Array body
+            // do a HTTP POST to https://bie.ala.org.au/species/bulklookup with a JSON Array body
             def jsonOutput = new JsonOutput()
             def jsonBody = jsonOutput.toJson(guids)
 
             //do the http POST
             HttpClient http = new DefaultHttpClient()
-            HttpPost post = new HttpPost("http://bie.ala.org.au/species/guids/bulklookup.json")
+            HttpPost post = new HttpPost("https://bie.ala.org.au/species/guids/bulklookup.json")
             post.setEntity(new StringEntity(jsonBody))
             def queryResponse = http.execute(post)
             def responseAsString = null
@@ -254,7 +254,7 @@ class ProxyController {
     }
 
     def occurrenceSearch = {
-        def url = ("http://biocache.ala.org.au/ws/occurrences/search?q=" + params.q + "&fq=geospatial_kosher%3Atrue&facets=" + params.facets + "&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + '&fsort=index&flimit=300').toURL()
+        def url = ("https://biocache-ws.ala.org.au/ws/occurrences/search?q=" + params.q + "&fq=geospatial_kosher%3Atrue&facets=" + params.facets + "&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&start=" + params.start + "&pageSize=" + params.pageSize + '&fsort=index&flimit=300').toURL()
         response.setContentType("application/json")
         render url.getText()
     }
@@ -264,13 +264,13 @@ class ProxyController {
         def start = params.start
         if (!pageSize) pageSize = 50
         if (!start) start = 0
-        def url = ("http://biocache.ala.org.au/ws/occurrences/search?q=*:*&fq=multimedia:Image&facet=off&&sort=first_loaded_date&dir=desc&pageSize=" + pageSize + "&start=" + start).toURL()
+        def url = ("https://biocache-ws.ala.org.au/ws/occurrences/search?q=*:*&fq=multimedia:Image&facet=off&&sort=first_loaded_date&dir=desc&pageSize=" + pageSize + "&start=" + start).toURL()
         response.setContentType("application/json")
         render url.getText()
     }
 
     def exploreSubgroup = {
-        def fullUrl = "http://biocache.ala.org.au/ws/occurrences/search?q=*:*&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&pageSize=0&facets=species_guid&flimit=2000&fq=species_subgroup:" + URLEncoder.encode(params.subgroup, "UTF-8")
+        def fullUrl = "https://biocache-ws.ala.org.au/ws/occurrences/search?q=*:*&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&pageSize=0&facets=species_guid&flimit=2000&fq=species_subgroup:" + URLEncoder.encode(params.subgroup, "UTF-8")
         try {
             def url = (fullUrl).toURL()
             def slurper = new JsonSlurper()
@@ -288,7 +288,7 @@ class ProxyController {
             def jsonOutput = new JsonOutput()
             def jsonBody = jsonOutput.toJson(names)
             HttpClient http = new DefaultHttpClient()
-            HttpPost post = new HttpPost("http://bie.ala.org.au/species/guids/bulklookup.json")
+            HttpPost post = new HttpPost("https://bie.ala.org.au/species/guids/bulklookup.json")
             post.setEntity(new StringEntity(jsonBody))
             def queryResponse = http.execute(post)
             def responseAsString = null
@@ -309,7 +309,7 @@ class ProxyController {
      * Biocache query and organisation by higher level groups
      */
     def searchByMultiRanks = {
-        def url = ("http://biocache.ala.org.au/ws/occurrences/search?q=*:*&fq=geospatial_kosher%3Atrue&facets=species_subgroup&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&pageSize=0&flimit=1000").toURL()
+        def url = ("https://biocache-ws.ala.org.au/ws/occurrences/search?q=*:*&fq=geospatial_kosher%3Atrue&facets=species_subgroup&lat=" + params.lat + "&lon=" + params.lon + "&radius=" + params.radius + "&pageSize=0&flimit=1000").toURL()
         response.setContentType("application/json")
         def slurper = new JsonSlurper()
         def result = slurper.parseText(url.getText())
